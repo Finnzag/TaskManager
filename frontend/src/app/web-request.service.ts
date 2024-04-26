fa4f527e608158a1c85ae45d210f604c994369a2
Finnzag <finndmad@gmail.com>
2024-04-26 15:05:34 +1200

    Updated root URL
    updated root URL to point it at where I am hosting the backend.

--- frontend/src/app/web-request.service.ts
+++ frontend/src/app/web-request.service.ts
@@ -9,7 +9,7 @@ export class WebRequestService {
   readonly ROOT_URL;
 
   constructor(private http: HttpClient) { 
-    this.ROOT_URL = 'http://localhost:3000';
+    this.ROOT_URL = 'https://taskmanager-vv8z.onrender.com';
   }
 
   get(uri: string) {
