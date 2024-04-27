d4a5498ed113f60a7c0d73eec37c19d563954aee
Finnzag <finndmad@gmail.com>
2024-04-27 16:14:57 +1200

    Fixed redirection on signup
    Fixed redirection on signup and a spelling error

--- frontend/src/app/pages/signup-page/signup-page.component.ts
+++ frontend/src/app/pages/signup-page/signup-page.component.ts
@@ -1,6 +1,7 @@
 import { HttpResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { AuthService } from '../../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup-page',
@@ -9,11 +10,12 @@ import { AuthService } from '../../auth.service';
 })
 export class SignupPageComponent {
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   onSignupButtonClicked(email: string, password: string) {
     this.authService.signup(email, password).subscribe((res: HttpResponse<any>) => {
       console.log(res);
+      this.router.navigate(['/lists']);
     })
   }
 }
